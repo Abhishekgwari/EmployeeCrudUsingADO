f2b73c261144865960f0decea9fe3a8dc1fee6c1
Abhishek <Abhishekgwari14sep@gmail.com>
2024-12-13 23:26:36 +0530

    Add project files.

---
+++ EmployeeCrudUsingADO/ConnectionString.cs
@@ -0,0 +1,10 @@
+﻿ namespace EmployeeCrudUsingADO
+{
+    public static class ConnectionString
+    {
+        private static string cs = "Server=ABHISHEK_ASUS\\SQLEXPRESS; Database= CrudADOdb; Trusted_Connection=True;";
+    
+    public static string dbcs { get => cs; }
+    
+    }
+}
